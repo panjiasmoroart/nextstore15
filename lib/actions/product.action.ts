f8cecb09b6cc633cd97f561cddcebce9a6734c45
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-22 20:43:23 +0700

    AppCreation & Basic Layout - Product Details Page

--- lib/actions/product.action.ts
+++ lib/actions/product.action.ts
@@ -34,3 +34,10 @@ export async function getLatestProducts(): Promise<ProductTypes[]> {
     createdAt: product.createdAt.toISOString(),
   }));
 }
+
+// Get single product by it's slug
+export async function getProductBySlug(slug: string) {
+  return await prisma.product.findFirst({
+    where: { slug: slug },
+  });
+}
