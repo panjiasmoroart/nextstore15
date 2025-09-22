4b319e508c9835a3618eaea70354b09a321a110a
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-22 12:58:56 +0700

    AppCreation & Basic Layout - Load Products From Database

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,13 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+// 👉 Jadi intinya:
+// Sebelum convert → ada Prisma.Decimal, Date (raw JS object).
+// Sesudah convert → semuanya jadi string/number biasa → aman dikirim ke client.
+
+// Convert prisma object into a regular JS object
+export function convertToPlainObject<T>(value: T): T {
+  return JSON.parse(JSON.stringify(value));
+}
+
