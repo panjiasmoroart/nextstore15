94f44a68a600f84056229dc2ca4372949c1db027
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-23 14:13:06 +0700

    Authentication - User Button & Sign Out

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -9,7 +9,7 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-// import UserButton from "./user-button";
+import UserButton from "./user-button";
 
 const Menu = () => {
   return (
@@ -21,7 +21,7 @@ const Menu = () => {
             <ShoppingCart /> Cart
           </Link>
         </Button>
-        {/* <UserButton /> */}
+        <UserButton />
       </nav>
       <nav className="md:hidden">
         <Sheet>
@@ -36,12 +36,7 @@ const Menu = () => {
                 <ShoppingCart /> Cart
               </Link>
             </Button>
-            <Button asChild>
-              <Link href="/cart">
-                <UserIcon /> Sign In
-              </Link>
-            </Button>
-            {/* <UserButton /> */}
+            <UserButton />
             <SheetDescription></SheetDescription>
           </SheetContent>
         </Sheet>
