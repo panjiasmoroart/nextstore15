a3e6efc7d430385d53253201d8c544b5b30d2055
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-25 23:44:09 +0700

    Authentication - Protecting Paths

--- auth.ts
+++ auth.ts
@@ -119,6 +119,23 @@ export const config = {
       return session;
     },
     authorized({ request, auth }) {
+      // Array of regex patterns of paths we want to protect
+      const protectedPaths = [
+        /\/shipping-address/,
+        /\/payment-method/,
+        /\/place-order/,
+        /\/profile/,
+        /\/user\/(.*)/,
+        /\/order\/(.*)/,
+        /\/admin/,
+      ];
+
+      // Get pathname from the req URL object
+      const { pathname } = request.nextUrl;
+
+      // Check if user is not authenticated and accessing a protected path
+      if (!auth && protectedPaths.some((p) => p.test(pathname))) return false;
+
       // check for session cart cookie 
       if (!request.cookies.get('sessionCartId')) {
         // generate new session cart id cookie 
