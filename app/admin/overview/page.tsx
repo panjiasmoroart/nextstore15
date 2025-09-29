db4f2214d95fa9c35b10536b5e17d687d2135ff2
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-29 14:42:18 +0700

    Admin Overview & Orders - Admin Layout & Main Nav

---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,11 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Admin Dashboard",
+};
+
+const AdminOverviewPage = () => {
+  return <div>AdminOverviewPage</div>;
+};
+
+export default AdminOverviewPage;
