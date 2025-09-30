99cacb595e4ac7f46458099285acfe3ee3fcc8de
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-30 21:19:46 +0700

    Admin Users & Search - Admin Order Search

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -22,19 +22,20 @@ export const metadata: Metadata = {
 const AdminOrdersPage = async (props: {
   searchParams: Promise<{ page: string; query: string }>;
 }) => {
-  const { page = "1" } = await props.searchParams;
+  const { page = "1", query: searchText } = await props.searchParams;
 
   await requireAdmin();
 
   const orders = await getAllOrders({
     page: Number(page),
+    query: searchText,
   });
 
   return (
     <div className="space-y-2">
       <div className="flex items-center gap-3">
         <h1 className="h2-bold">Orders</h1>
-        {/* {searchText && (
+        {searchText && (
           <div>
             Filtered by <i>&quot;{searchText}&quot;</i>{" "}
             <Link href="/admin/orders">
@@ -43,7 +44,7 @@ const AdminOrdersPage = async (props: {
               </Button>
             </Link>
           </div>
-        )} */}
+        )}
       </div>
       <div className="overflow-x-auto">
         <Table>
