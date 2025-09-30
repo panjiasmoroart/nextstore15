476a9a2f4a9cc6d5d7d2dad3b0bfb76e1748bf17
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-30 12:03:44 +0700

    Admin Products & Image Upload - Get Products For Admin Page

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,60 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { getAllProducts } from "@/lib/actions/product.actions";
+import { requireAdmin } from "@/lib/auth-guard";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const AdminProductsPage = async (props: {
+  searchParams: Promise<{
+    page: string;
+    query: string;
+    category: string;
+  }>;
+}) => {
+  await requireAdmin();
+
+  const searchParams = await props.searchParams;
+
+  const page = Number(searchParams.page) || 1;
+  const searchText = searchParams.query || "";
+  const category = searchParams.category || "";
+
+  const products = await getAllProducts({
+    query: searchText,
+    page,
+    category,
+  });
+
+  return (
+    <div className="space-y-2">
+      <div className="flex-between">
+        <div className="flex items-center gap-3">
+          <h1 className="h2-bold">Products</h1>
+          {searchText && (
+            <div>
+              Filtered by <i>&quot;{searchText}&quot;</i>{" "}
+              <Link href="/admin/products">
+                <Button variant="outline" size="sm">
+                  Remove Filter
+                </Button>
+              </Link>
+            </div>
+          )}
+        </div>
+        <Button asChild variant="default">
+          <Link href="/admin/products/create">Create Product</Link>
+        </Button>
+      </div>
+      <Table>{JSON.stringify(products)}</Table>
+    </div>
+  );
+};
+
+export default AdminProductsPage;
--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -4,8 +4,9 @@
 // import { convertToPlainObject } from '../utils';
 // import { PrismaClient } from '@prisma/client'
 import { prisma } from '@/db/prisma';
-import { LATEST_PRODUCTS_LIMIT } from '../constants';
+import { LATEST_PRODUCTS_LIMIT, PAGE_SIZE } from '../constants';
 import { ProductTypes } from "@/types";
+import { Prisma } from '@prisma/client';
 
 // const prisma = new PrismaClient();
 
@@ -40,4 +41,30 @@ export async function getProductBySlug(slug: string) {
   return await prisma.product.findFirst({
     where: { slug: slug },
   });
+}
+
+// Get all products
+export async function getAllProducts({
+  query,
+  limit = PAGE_SIZE,
+  page,
+  category,
+}: {
+  query: string;
+  limit?: number;
+  page: number;
+  category?: string;
+}) {
+
+  const data = await prisma.product.findMany({
+    skip: (page - 1) * limit,
+    take: limit,
+  });
+
+  const dataCount = await prisma.product.count();
+
+  return {
+    data,
+    totalPages: Math.ceil(dataCount / limit),
+  };
 }
