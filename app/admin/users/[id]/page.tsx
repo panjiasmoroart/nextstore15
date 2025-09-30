04502667bb52d61878518cfdaa47a39afd6f5333
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-30 21:02:31 +0700

    Admin Users & Search - Update User Form

--- app/admin/users/[id]/page.tsx
+++ app/admin/users/[id]/page.tsx
@@ -1,7 +1,7 @@
 import { Metadata } from "next";
 import { notFound } from "next/navigation";
 import { getUserById } from "@/lib/actions/user.actions";
-// import UpdateUserForm from "./update-user-form";
+import UpdateUserForm from "./update-user-form";
 import { requireAdmin } from "@/lib/auth-guard";
 
 export const metadata: Metadata = {
@@ -24,8 +24,7 @@ const AdminUserUpdatePage = async (props: {
   return (
     <div className="space-y-8 max-w-lg mx-auto">
       <h1 className="h2-bold">Update User</h1>
-      <p>{JSON.stringify(user)}</p>
-      {/* <UpdateUserForm user={user} /> */}
+      <UpdateUserForm user={user} />
     </div>
   );
 };
