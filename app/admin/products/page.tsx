eb6c6e2066e1dc24b67eca6dfc5de8aed894cb1c
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-09-30 12:08:31 +0700

    Admin Products & Image Upload - Display Products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -10,6 +10,8 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import { formatCurrency, formatId } from "@/lib/utils";
+import Pagination from "@/components/shared/pagination";
 
 const AdminProductsPage = async (props: {
   searchParams: Promise<{
@@ -52,7 +54,42 @@ const AdminProductsPage = async (props: {
           <Link href="/admin/products/create">Create Product</Link>
         </Button>
       </div>
-      <Table>{JSON.stringify(products)}</Table>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>ID</TableHead>
+            <TableHead>NAME</TableHead>
+            <TableHead className="text-right">PRICE</TableHead>
+            <TableHead>CATEGORY</TableHead>
+            <TableHead>STOCK</TableHead>
+            <TableHead>RATING</TableHead>
+            <TableHead className="w-[100px]">ACTIONS</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {products.data.map((product) => (
+            <TableRow key={product.id}>
+              <TableCell>{formatId(product.id)}</TableCell>
+              <TableCell>{product.name}</TableCell>
+              <TableCell className="text-right">
+                {formatCurrency(product.price)}
+              </TableCell>
+              <TableCell>{product.category}</TableCell>
+              <TableCell>{product.stock}</TableCell>
+              <TableCell>{product.rating}</TableCell>
+              <TableCell className="flex gap-1">
+                <Button asChild variant="outline" size="sm">
+                  <Link href={`/admin/products/${product.id}`}>Edit</Link>
+                </Button>
+                {/* DeleteDialog  */}
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {products.totalPages > 1 && (
+        <Pagination page={page} totalPages={products.totalPages} />
+      )}
     </div>
   );
 };
