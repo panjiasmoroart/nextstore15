7d70db92d80e30aff6067b8a4abb0bbd21660a65
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-01 12:07:08 +0700

    Search Filtering, Drawer & Carousel - Featured Product Caraousel

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,26 @@
 // import sampleData from "@/db/sample-data";
+import ProductCarousel from "@/components/shared/product/product-carousel";
 import ProductList from "@/components/shared/product/product-list";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getFeaturedProducts,
+  getLatestProducts,
+} from "@/lib/actions/product.actions";
 
 const Homepage = async () => {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
+
+  const parseFeaturedProducts = featuredProducts.map((product) => ({
+    ...product,
+    price: parseFloat(product.price),
+    rating: product.rating,
+  }));
 
   return (
     <>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={parseFeaturedProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" />
     </>
   );
