8dd18bd5b988bfc1fbfa610980119395d00686ae
panjiasmoroart <pandjiasmoroart@gmail.com>
2025-10-05 22:03:05 +0700

    Email Purchase Receipts - Sending Emails (Fixed)

--- email/index.tsx
+++ email/index.tsx
@@ -10,10 +10,24 @@ import PurchaseReceiptEmail from "./purchase-receipt";
 const resend = new Resend(process.env.RESEND_API_KEY!);
 
 export const sendPurchaseReceipt = async ({ order }: { order: Order }) => {
-  await resend.emails.send({
-    from: `${APP_NAME} <${SENDER_EMAIL}>`,
-    to: order.user.email,
-    subject: `Order Confirmation ${order.id}`,
-    react: <PurchaseReceiptEmail order={order} />,
-  });
+  // await resend.emails.send({
+  //   from: `${APP_NAME} <${SENDER_EMAIL}>`,
+  //   to: order.user.email,
+  //   subject: `Order Confirmation ${order.id}`,
+  //   react: <PurchaseReceiptEmail order={order} />,
+  // });
+  try {
+    console.log("Sending email to:", order.user.email);
+
+    const result = await resend.emails.send({
+      from: `${APP_NAME} <${SENDER_EMAIL}>`,
+      to: "tarxvfextract@gmail.com",
+      subject: `Order Confirmation ${order.id}`,
+      react: <PurchaseReceiptEmail order={order} />,
+    });
+
+    console.log("Email send result:", result);
+  } catch (error) {
+    console.error("Failed to send email:", error);
+  }
 };
